9962c05f65fed175eef51a559dd74ff3ee5cecba
DeorixProg <ma2005ksi@gmail.com>
2025-07-15 12:17:32 +0300

    Add project files.

---
+++ RoadTo.NetDeveloperCarier/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoadTo.NetDeveloperCarier.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
