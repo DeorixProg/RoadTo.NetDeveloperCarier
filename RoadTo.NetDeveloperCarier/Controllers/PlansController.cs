6a974b801f119c1af3975826c896acc1603b2000
DeorixProg <ma2005ksi@gmail.com>
2025-07-15 13:41:56 +0300

    Create plans controlet an weo pages

---
+++ RoadTo.NetDeveloperCarier/Controllers/PlansController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoadTo.NetDeveloperCarier.Controllers
+{
+    public class PlansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
